a92652f275a944327864ec86f06d3fc5955a1982
Ridwan Maulana Tanjung <rdtanx@gmail.com>
2025-04-11 20:15:39 +0700

    reconfigure nextjs app

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,18 @@
 from fastapi import Depends, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .data import router as data_router
 from .ai import router as ai_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(data_router.router, prefix="/api/sales-reps", tags=["sales-reps"])
 app.include_router(ai_router.router, prefix="/api/ai", tags=["ai"])
